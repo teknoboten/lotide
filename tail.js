665e1abba23afad0bdb4169d3c464de2a3a8f77a
modemgirl <serra@modemgirl.ca>
2022-01-29 17:37:55 +0000

    cleaned up tail.js after feedback session / tinkering / linting

--- tail.js
+++ tail.js
@@ -6,25 +6,24 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-// const tail = (arr) => {
-//   let newArr = [];
-//   for (let t = 1; t < arr.length; t++) {
-//     newArr.push(arr[t]);
-//   }
-//   return newArr;
-// };
-
 const tail = (arr) => {
-    return arr.slice(1) 
-  };
+  return arr.slice(1);
+};
 
 const roseFam = ["david", "moira", "alexis", "johnny"];
-const emptyArr = [];
-const lonelyElement = ["stevie"];
+// const emptyArr = [];
+// const lonelyElement = ["stevie"];
 
-//test to make sure original array is not modified after tail is run
-console.log(`check the orignal array is not modified:`);
-assertEqual(roseFam.length, 4);
+let result = tail(roseFam);
+let expected = ["moira", "alexis", "johnny"];
+
+const checkResult = (res, ex) => {
+  for (let i = 0; i < res.length; i++) {
+    assertEqual(res[i], ex[i]);
+  }
+};
+
+checkResult(result, expected);
 
 //test for empty array
 // console.log(`empty array:`);
@@ -36,20 +35,6 @@ assertEqual(roseFam.length, 4);
 
 // const result = tail(["david", "moira", "alexis", "johnny"]);
 
-
-
-// workaround as suggested by the notes that does not involve comparing arrays!
-assertEqual(result.length, 3);
-assertEqual(result[0], "moira");
-assertEqual(result[1], "alexis");
-assertEqual(result[2], "johnny");
-
-// my inital solution
-let result = tail(["david", "moira", "alexis", "johnny"]);
-let expected = ["moira", "alexis", "johnny"];
-
-const checkResult = (res, ex) => {
-  for (let i = 0; i < res.length; i++) {
-    assertEqual(res[i], ex[i]);
-  }
-};
+//test to make sure original array is not modified after tail is run
+// console.log(`check the orignal array is not modified:`);
+// assertEqual(roseFam.length, 4);
