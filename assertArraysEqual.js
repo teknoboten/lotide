6cc53c657bf19ae794cd9876a2a14446d3777628
modemgirl <serra@modemgirl.ca>
2022-01-29 17:40:56 +0000

    re-linted

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -17,9 +17,9 @@ const eqArrays = (arr1, arr2) => {
 //test arrays
 let arrA = ["david", "moira", "alexis", "johnny"];
 let arrB = ["david", "moira", "alexis", "johnny"];
-let arrC = ["moira", "alexis", "johnny"];
-let arrD = [];
-let arrE = ["stevie", "patrick", "twyla", "jocelyn"];
+// let arrC = ["moira", "alexis", "johnny"];
+// let arrD = [];
+// let arrE = ["stevie", "patrick", "twyla", "jocelyn"];
 
 /*
 inputs: two arrays || desired outputs: appropriate message logged to console
@@ -34,6 +34,5 @@ const assertArraysEqual = (arr1, arr2) => {
   }
 };
 
-assertArraysEqual(arrD, arrE);
+assertArraysEqual(arrA, arrB);
 
-//bug!! two empty arrays === true!
