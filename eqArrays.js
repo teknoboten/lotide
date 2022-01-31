b31c605e5634ac770c14559db5a5b9d08a18a0c0
modemgirl <serra@modemgirl.ca>
2022-01-31 22:08:16 +0000

    fix for empty array bug

--- eqArrays.js
+++ eqArrays.js
@@ -9,28 +9,40 @@ const assertEqual = (actual, expected) => {
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = (arr1, arr2) => {
   //loop through arrays to check each el matches
-  let result = false;
+  // let result = false;
   let count = 0;
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (const x of arr1) {
     if (x !== arr2[count]) {
-      result = false;
-      return result;
+      return false;
     } else {
-      result = true;
       count += 1;
     }
   }
-  return result;
+  return true;
 };
 
 //test arrays
 let arrA = ["david", "moira", "alexis", "johnny"];
 let arrB = ["david", "moira", "alexis", "johnny"];
-let arrC = ["moira", "alexis", "johnny"];
 let arrD = [];
 let arrE = ["stevie", "patrick", "twyla", "jocelyn"];
 
-assertEqual(eqArrays(arrA, arrB), true); //should PASS
-assertEqual(eqArrays(arrA, arrC), false); //should PASS
-assertEqual(eqArrays(arrA, arrE), false);  //should FAIL
-assertEqual(eqArrays(arrD, arrE), false); //should PASS
+// test cases
+console.log(`matching arrays. expected: true`);
+assertEqual(eqArrays(arrA, arrB), true);
+
+console.log(`\nnon-matching arrays. expected: false`);
+assertEqual(eqArrays(arrA, arrE), false);
+
+console.log(`\nfirst array empty. expected: false`);
+assertEqual(eqArrays(arrD, arrE), false);
+
+console.log(`\n second array empty. expected: false`);
+assertEqual(eqArrays(arrE, arrD), false);
+
+console.log(`\ntwo empty arrays. expected: true`);
+assertEqual(eqArrays(arrD, arrD), true);
