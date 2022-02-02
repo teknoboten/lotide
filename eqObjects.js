701bcbb11fc93598110226921eafe9254c62155b
modemgirl <serra@modemgirl.ca>
2022-02-02 19:37:53 +0000

    change object.entries to object.keys

--- eqObjects.js
+++ eqObjects.js
@@ -27,32 +27,24 @@ const eqArrays = (arr1, arr2) => {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 
-// console.log(obj1[k]); //returns value: 1
-// console.log(k); // returns key: a
-
 const eqObjects = (obj1, obj2) => {
-  let isArray = false;
 //check if both objects have the same number of keys
-  if (Object.entries(obj1).length !== Object.entries(obj2).length){
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   }
-  for (k in obj1) { 
+  for (const k in obj1) {
     if (!obj2[k]) {   //if k does not exist in obj2, return false
       return false;
     }
     if (Array.isArray(obj1[k]) && Array.isArray(obj2[k])) {   // check if k is an array
-      isArray = true;
       if (!eqArrays(obj1[k], obj2[k])) {
         return false;
-      };
-    }
-    if (!isArray){
+      }
+    }  else {
       if (obj1[k] !== obj2[k]) {    //if obj2[k] !== obj2[k], return false
-        // console.log(obj1[k]);
-        // console.log(obj2[k]);
         return false;
       }
-    } 
+    }
   }
   return true;
 };
@@ -61,17 +53,14 @@ const eqObjects = (obj1, obj2) => {
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
-const cc = { c: "1", d: "3" };
 
 assertEqual(eqObjects(cd, cd2), false);
 assertEqual(eqObjects(cd, dc), true);
 
-//primitive test cases 
+//primitive test cases
 // const ab = { a: "1", b: "2"};
 // const ba = { b: "2", a: "1"};
 // const abc = { a: "1", b: "2", c: "3"};
 
 // assertEqual(eqObjects(ab, ba), true);
 // assertEqual(eqObjects(ab, abc), false);
-
-//question -- WHY 
