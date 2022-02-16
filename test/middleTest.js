db063b584e8a98ede79902e4b72800173f98187a
modemgirl <serra@modemgirl.ca>
2022-02-16 00:00:13 +0000

    refactor middle

---
+++ test/middleTest.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+
+console.log(`testing middle:\n`);
+
+//test arrays
+let singleArr = ["i'm lonely"];
+let evenArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let oddArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+console.log(`array with 1 or 2 elements:`);
+assertArraysEqual(middle(singleArr), []);
+
+console.log(`\ntesting an even array:`);
+assertArraysEqual(middle(evenArr), [ 5, 6 ]);
+
+console.log(`\ntesting an odd array:`);
+assertArraysEqual(middle(oddArr), [ 6 ]);
+
