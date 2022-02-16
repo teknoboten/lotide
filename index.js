a3cb25f158c825d54a69831be076207e73b710b7
modemgirl <serra@modemgirl.ca>
2022-02-16 01:10:16 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+// const countLetters = require('./countLetters');
+// const countOnly = require('./countOnly');
+// const assertObjectsEqual = require('./assertObjectsEqual');
+// const findKey = require('./findKey');
+// const findKeyByValue = require('./findKeyByValue');
+// const flatten = require('./flatten');
+// const letterPositions = require('./letterPositions');
+// const map = require('./map');
+// const takeUntil = require('./takeUntil');
+// const without = require('./without');
+
+module.exports = { head, tail, middle, assertArraysEqual, assertEqual, eqArrays };
